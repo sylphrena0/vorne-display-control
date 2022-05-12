d3344af095c95044dd58c45d8341e03c8c40efc6
Nightwielder1 <54223569+Nightwielder1@users.noreply.github.com>
2022-05-12 12:00:06 -0400

    added a logging table

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS msg;
+DROP TABLE IF EXISTS logging;
 
 CREATE TABLE user ( --create user table
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,9 +16,16 @@ CREATE TABLE msg ( --create settings table
   df INT NOT NULL
 );
 
+CREATE TABLE logging ( --create logging table
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  datetime TEXT NOT NULL,
+  lvl TEXT NOT NULL,
+  msg TEXT NOT NULL
+);
+
 INSERT INTO msg VALUES( --insert default values
   1, --id
   "Welcome to North Mountain Supply", --msg
   "Medium Scrolling", --mode
   0 --df
-)
+);
