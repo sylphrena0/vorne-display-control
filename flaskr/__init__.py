7f5ddb73e6b6350b49221581a4db392249204922
Syl <kirkkleinsasser@gmail.com>
2022-05-17 12:30:44 -0400

    updated default secret key

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='TeAFL25rAaZutN8lloICx9Tm3cKZl7JEvKvu7gne4dNRKNFVATMuAN3Us3z5NKTd',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
     
