d7a0be013022ff0dffa29a0b025eea1b89f70f1d
Syl <kirkkleinsasser@gmail.com>
2022-05-17 14:07:20 -0400

    seperate delete log statement for DEBUG

--- flaskr/db.py
+++ flaskr/db.py
@@ -22,7 +22,8 @@ def log(level,message):
     int_level = ["DEBUG","INFO","WARN","ERROR","CRIT"].index(level.upper())
     try:
         db = get_db()
-        db.execute("DELETE FROM logging WHERE id NOT IN (SELECT id FROM logging ORDER BY id DESC LIMIT 200)") #remove old logs
+        db.execute("DELETE FROM logging WHERE id NOT IN (SELECT id FROM logging WHERE lvl <> 0 ORDER BY id DESC LIMIT 200) AND lvl <> 0") #remove old logs that aren't DEBUG
+        db.execute("DELETE FROM logging WHERE id NOT IN (SELECT id FROM logging WHERE lvl = 0 ORDER BY id DESC LIMIT 100) AND lvl = 0") #remove old logs that are DEBUG
         db.commit()
         db.execute("INSERT INTO logging (datetime, lvl, msg) VALUES (?, ?, ?)", (timestamp, int_level, message), )
         print(timestamp,"-",message)
