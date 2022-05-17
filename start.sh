8a6b6e26d50f9a62d254d2181270d2abbb025f4a
Syl <kirkkleinsasser@gmail.com>
2022-05-17 15:15:17 -0400

    reenabled flask debugging

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 export FLASK_APP=flaskr
 export FLASK_ENV=development
-flask run -h 0.0.0.0 -p 80 --no-reload --no-debugger
+flask run -h 0.0.0.0 -p 80 --no-reload
+#gotta disable reloading, otherwise multiple backends will be started at once and can cause errors
+#add --no-debugger if this application is open networks with untrusted users
